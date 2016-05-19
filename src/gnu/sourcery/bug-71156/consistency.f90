356b47f4ff1cb16caedd4270a1ce3edb75b5b0b2
Damian Rouson <damian@sourceryinstitute.org>
2016-05-19 16:23:01 -0700

    Add gfortran PURE interf/implem inconsistency bug

---
+++ src/gnu/sourcery/bug-71156/consistency.f90
@@ -0,0 +1,14 @@
+module my_interface
+  implicit none
+  interface
+    pure module subroutine f
+    end subroutine
+  end interface
+end module 
+
+submodule(my_interface) my_implementation
+  implicit none
+contains
+    pure module subroutine f
+    end subroutine
+end submodule
