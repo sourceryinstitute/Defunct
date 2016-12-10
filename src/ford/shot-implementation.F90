5d8213a3b199fe6d2f2f57f6e3e7f8c64827efee
Damian Rouson <damian@sourceryinstitute.org>
2016-12-09 22:32:41 -0800

    simplify FORD bug report

--- src/ford/shot-implementation.F90
+++ src/ford/shot-implementation.F90
@@ -2,8 +2,6 @@ submodule(shot_interface) shot_implementation
   implicit none
 contains
   module procedure from_the_field
-     use parabola_interface, only : parabola
-     use physical_constants ,only : g
 #ifndef FORD
        this%parabola = parabola()
 #endif
--- src/ford/shot-interface.f90
+++ src/ford/shot-interface.f90
@@ -2,14 +2,13 @@ module shot_interface
   use parabola_interface, only : parabola
   implicit none
   type ,extends(parabola) :: shot
-    real(rkind) :: distance
   contains
     procedure :: from_the_field
   end type
   interface
     pure module subroutine from_the_field(this)
        implicit none
-       class(shot) ,intent(inout) :: this
+       class(shot) :: this
     end subroutine
    end interface
 end module
