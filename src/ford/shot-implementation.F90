944fac3eb69b60c8bd0977719f713a145cb4b49f
Damian Rouson <damian@sourceryinstitute.org>
2016-12-09 22:30:19 -0800

    Further simplify FORD bug

--- src/ford/shot-implementation.F90
+++ src/ford/shot-implementation.F90
@@ -4,10 +4,8 @@ contains
   module procedure from_the_field
      use parabola_interface, only : parabola
      use physical_constants ,only : g
-     associate(y0=>release_height,u => speed*cos(inclination),v => speed*sin(inclination))
 #ifndef FORD
-       this%parabola = parabola( a=-g/(2*u**2), b=v/u ,c=y0)
+       this%parabola = parabola()
 #endif
-     end associate
   end procedure
 end submodule
--- src/ford/shot-interface.f90
+++ src/ford/shot-interface.f90
@@ -1,21 +1,15 @@
 module shot_interface
-  use kind_parameters ,only : rkind,ikind
   use parabola_interface, only : parabola
   implicit none
   type ,extends(parabola) :: shot
     real(rkind) :: distance
   contains
-    procedure, private :: from_the_field
-    generic :: set_trajectory => from_the_field
+    procedure :: from_the_field
   end type
   interface
-    pure module subroutine from_the_field(this,inclination,speed,release_height,range_)
+    pure module subroutine from_the_field(this)
        implicit none
        class(shot) ,intent(inout) :: this
-       real(rkind) ,intent(in) :: inclination
-       real(rkind) ,intent(in) :: speed
-       real(rkind) ,intent(in) :: release_height
-       real(rkind) ,intent(in) :: range_
     end subroutine
    end interface
 end module
