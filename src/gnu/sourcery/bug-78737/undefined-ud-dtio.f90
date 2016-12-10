f336765e15391262873a715fe4c8697f8e5092f8
Damian Rouson <damian@sourceryinstitute.org>
2016-12-09 17:35:42 -0800

    Improved GNU UDDTIO bug report

--- src/gnu/sourcery/bug-78737/undefined-ud-dtio.f90
+++ src/gnu/sourcery/bug-78737/undefined-ud-dtio.f90
@@ -15,7 +15,19 @@ module object_interface
       character (len=*), intent(inout) :: iomsg
     end subroutine
   end interface
+  type, extends(object) :: non_abstract_child 
+  contains
+    procedure :: write_formatted
+  end type
 contains
+  subroutine write_formatted(this,unit,iotype,vlist,iostat,iomsg)
+    class(non_abstract_child), intent(in) :: this
+    integer, intent(in) :: unit
+    character (len=*), intent(in) :: iotype
+    integer, intent(in) :: vlist(:)
+    integer, intent(out) :: iostat
+    character (len=*), intent(inout) :: iomsg
+  end subroutine
   subroutine assert(a)
     class(object):: a
     write(*,*) a 
