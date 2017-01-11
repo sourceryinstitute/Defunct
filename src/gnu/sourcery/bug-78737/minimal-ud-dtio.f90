555bebb8c926d929297d8ccd886d0b583ff27ded
Damian Rouson <damian@rouson.net>
2017-01-11 15:28:07 -0800

    Add GNU UD-DTIO bug.

---
+++ src/gnu/sourcery/bug-78737/minimal-ud-dtio.f90
@@ -0,0 +1,28 @@
+module object_interface
+  logical :: fortran_rocks=.true.
+  type, abstract :: object
+  contains
+    procedure(write_formatted_interface), deferred ::write_formatted 
+    generic :: write(formatted)=>write_formatted
+  end type 
+  abstract interface
+    subroutine write_formatted_interface(this,unit,iotype,vlist,iostat,iomsg)
+      import object
+      class(object), intent(in) :: this
+      integer, intent(in) :: unit
+      character (len=*), intent(in) :: iotype
+      integer, intent(in) :: vlist(:)
+      integer, intent(out) :: iostat
+      character (len=*), intent(inout) :: iomsg
+    end subroutine
+  end interface
+contains
+  subroutine assert(a)
+    class(object):: a
+    write(*,*) a 
+  end subroutine
+end module
+   
+  use object_interface, only : fortran_rocks
+  if (fortran_rocks) print *,"Rock on."
+end
