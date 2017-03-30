be136b65d94812a1ed74c9c5ec35c3c7994f77d3
Damian Rouson <damian@sourceryinstitute.org>
2017-03-29 21:04:00 -0700

    Eliminated FGR_Mesh.f90

--- src/gnu/nrc/call-burst-release/FGR_Mesh.f90
+++
@@ -1,8 +0,0 @@
-MODULE FGR_Mesh
-    USE Kinds, ONLY : ipk, r8k
-    IMPLICIT NONE
-    TYPE :: FGR_Node
-    END TYPE FGR_Node
-    TYPE, EXTENDS (FGR_Node) :: FRAPFGR_Node
-    END TYPE
-END MODULE FGR_Mesh
