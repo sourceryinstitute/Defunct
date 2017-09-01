a41c3af26c1a3db1362f236098d3d08dbf3341b4
Damian Rouson <damian@sourceryinstitute.org>
2017-08-31 22:40:05 -0700

    Add original NRC bug report files

---
+++ src/gnu/originals/assemble.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cp `find ../FRAPCON4.1/ -name FissionGas.f90` .
+cp `find ../FRAPCON4.1/ -name Functions.f90` .
+cp `find ../FRAPCON4.1/ -name FGR_Mesh.f90` .
+cp `find ../FRAPCON4.1/ -name Grid.f90` .
+cp `find ../FRAPCON4.1/ -name CoolantVars.f90` .
+cp `find ../FRAPCON4.1/ -name Refabrication.f90` .
+cp `find ../FRAPCON4.1/ -name Commons.f90` .
+cp `find ../FRAPCON4.1/ -name Conversions.f90` .
+cp `find ../FRAPCON4.1/ -name Variables.f90` .
+cp `find ../FRAPCON4.1/ -name GammaHeating.f90` .
+cp `find ../FRAPCON4.1/ -name Kind.f90` .
+cp `find ../FRAPCON4.1/ -name Mesh.f90` .
