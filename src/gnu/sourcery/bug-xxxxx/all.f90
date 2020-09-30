dc8ef2802483b53fc91c763aa308594702f53e70
Damian Rouson <damian@sourceryinstitute.org>
2020-09-30 13:26:40 -0700

    reduce array dimension and remove unused dummy arg

--- src/gnu/sourcery/bug-xxxxx/all.f90
+++ src/gnu/sourcery/bug-xxxxx/all.f90
@@ -2,18 +2,15 @@ module results_interface
   implicit none
 
   type results_t
-    real, allocatable :: output(:, :)
+    real, allocatable :: output(:)
   end type
 
 contains
 
-  pure module subroutine max_filtered_normalized_distance(this, rhs)
-    class(results_t), intent(in) :: this
-    type(results_t), intent(in) :: rhs
-    real, allocatable :: rhs_filtered(:,:)
-
+  subroutine max_filtered_normalized_distance(rhs)
+    type(results_t)rhs
+    real, allocatable :: rhs_filtered(:)
     rhs_filtered = rhs%output
-
   end subroutine
 
 end module
