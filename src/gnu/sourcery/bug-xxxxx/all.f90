0c7ba736074c5e5efcec641e2df8172958ffdc3d
Damian Rouson <damian@sourceryinstitute.org>
2020-09-30 13:23:42 -0700

    remove unused bindings & variables

--- src/gnu/sourcery/bug-xxxxx/all.f90
+++ src/gnu/sourcery/bug-xxxxx/all.f90
@@ -3,23 +3,17 @@ module results_interface
 
   type results_t
     real, allocatable :: output(:, :)
-  contains
-    procedure :: max_filtered_normalized_distance
   end type
 
 contains
 
-  pure module function max_filtered_normalized_distance(this, rhs)
+  pure module subroutine max_filtered_normalized_distance(this, rhs)
     class(results_t), intent(in) :: this
     type(results_t), intent(in) :: rhs
-    real :: max_filtered_normalized_distance
-    integer, parameter :: mdotos_column=4, thrust_column=5
     real, allocatable :: rhs_filtered(:,:)
-    type(results_t) distance
 
     rhs_filtered = rhs%output
 
-    max_filtered_normalized_distance = maxval(rhs%output)
-  end function
+  end subroutine
 
 end module
