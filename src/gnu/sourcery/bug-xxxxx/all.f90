8d129c83aa20fb829f5e8c7572cdf3661e9a0acd
Damian Rouson <damian@sourceryinstitute.org>
2020-09-30 13:18:34 -0700

    reduce indentation level to 2 spaces

--- src/gnu/sourcery/bug-xxxxx/all.f90
+++ src/gnu/sourcery/bug-xxxxx/all.f90
@@ -1,34 +1,31 @@
 module results_interface
+  implicit none
 
-    implicit none
-    private
+  type results_t
+    real, allocatable :: output(:, :)
+  contains
+    procedure :: max_filtered_normalized_distance
+  end type
 
-    type, public :: results_t
-        private
-        real, allocatable :: output(:, :)
-    contains
-        procedure :: max_filtered_normalized_distance
-    end type
-
-    interface
-        pure module function max_filtered_normalized_distance(this, rhs)
-            implicit none
-            class(results_t), intent(in) :: this
-            type(results_t), intent(in) :: rhs
-            real :: max_filtered_normalized_distance
-        end function
-    end interface
+  interface
+    pure module function max_filtered_normalized_distance(this, rhs)
+        implicit none
+        class(results_t), intent(in) :: this
+        type(results_t), intent(in) :: rhs
+        real :: max_filtered_normalized_distance
+    end function
+  end interface
 
 contains
 
-    module procedure max_filtered_normalized_distance
-        integer, parameter :: mdotos_column=4, thrust_column=5
-        real, allocatable :: rhs_filtered(:,:)
-        type(results_t) distance
+  module procedure max_filtered_normalized_distance
+    integer, parameter :: mdotos_column=4, thrust_column=5
+    real, allocatable :: rhs_filtered(:,:)
+    type(results_t) distance
 
-        rhs_filtered = rhs%output
+    rhs_filtered = rhs%output
 
-        max_filtered_normalized_distance = maxval(rhs%output)
-    end procedure
+    max_filtered_normalized_distance = maxval(rhs%output)
+  end procedure
 
 end module
