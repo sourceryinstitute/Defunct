1f40768b792b692a1b689e5f8884153d7d3205e7
Damian Rouson <damian@sourceryinstitute.org>
2020-09-30 13:19:36 -0700

    eliminate interface body

--- src/gnu/sourcery/bug-xxxxx/all.f90
+++ src/gnu/sourcery/bug-xxxxx/all.f90
@@ -7,18 +7,12 @@ module results_interface
     procedure :: max_filtered_normalized_distance
   end type
 
-  interface
-    pure module function max_filtered_normalized_distance(this, rhs)
-        implicit none
-        class(results_t), intent(in) :: this
-        type(results_t), intent(in) :: rhs
-        real :: max_filtered_normalized_distance
-    end function
-  end interface
-
 contains
 
-  module procedure max_filtered_normalized_distance
+  pure module function max_filtered_normalized_distance(this, rhs)
+    class(results_t), intent(in) :: this
+    type(results_t), intent(in) :: rhs
+    real :: max_filtered_normalized_distance
     integer, parameter :: mdotos_column=4, thrust_column=5
     real, allocatable :: rhs_filtered(:,:)
     type(results_t) distance
@@ -26,6 +20,6 @@ contains
     rhs_filtered = rhs%output
 
     max_filtered_normalized_distance = maxval(rhs%output)
-  end procedure
+  end function
 
 end module
